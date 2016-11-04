2f953b20145a6a419b669a8800e2780cec6f79ca
Paul Glavich <glav@theglavs.com>
2016-11-04 18:10:14 +1100

    Added a new header component via aurelia CLI

---
+++ src/resources/elements/header.js
@@ -0,0 +1,16 @@
+import {bindable} from 'aurelia-framework';
+
+export class Header {
+
+    constructor() {
+        this.appTitle = "Aurelia WebApp";
+        this.headerText = "Header element";
+    }
+
+  @bindable value;
+
+  valueChanged(newValue, oldValue) {
+
+  }
+}
+
